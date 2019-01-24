e6ecb5ee3e1b0b059ee785ec8af68379e453612c
Stanley Heryanto <stanleyheryanto@Gmail.com>
2019-01-24 18:52:44 +0700

    Double quotes

--- mail_out.php
+++ mail_out.php
@@ -33,7 +33,7 @@
             $sentTime = $dmail['sent_time'];
             $sentTime = substr($sentTime, 8, 2) . ':00 ' . substr($sentTime, 6, 2) . '-' . substr($sentTime, 4, 2) . '-' . substr($sentTime, 0, 4);
             $message = $dmail['message'];
-            $message = $message . '\n\n\n' . $dmail['sender'] . ' sent you this mail from ' . $sentTime . '.\nFor more information, see https://renziera.web.id/reverse-d-mail/';
+            $message = $message . "\n\n\n" . $dmail['sender'] . ' sent you this mail from ' . $sentTime . ".\nFor more information, see https://renziera.web.id/reverse-d-mail/";
             $status = wp_mail($dmail['target'], $dmail['subject'], $message);
             if($status){
                 $updateQuery = 'UPDATE dmail SET sent = 1 WHERE id = :id';
